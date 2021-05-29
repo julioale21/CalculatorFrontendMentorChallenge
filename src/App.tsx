a8ada60aa5de96dff153ddce346771ae59fbf7d6
Julio Romero <julio_ale21@hotmail.com>
2021-05-28 22:04:54 -0300

    chore: changed height

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Stack } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <Stack alignItems="center" backgroundColor="blue.800" height="100vh">
+    <Stack alignItems="center" backgroundColor="blue.800" height="full" paddingBottom={12}>
       <Calculator />
     </Stack>
   );
