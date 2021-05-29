774edf30b2f0a49db4dceaf3b1ff1c1806080456
Julio Romero <julio_ale21@hotmail.com>
2021-05-28 22:38:23 -0300

    chore: used custom hook and added events

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Stack, Grid, GridItem, Text } from "@chakra-ui/react";
 import Display from "./Display";
 import ButtonCalc from "./ButtonCalc";
+import useCalculator from "./useCalculator";
 
 const Calculator = () => {
+  const [data, handleClick] = useCalculator();
+
   return (
     <Stack width={{ base: "325px", md: "487px" }}>
       <Stack paddingTop={24}>
@@ -14,7 +17,7 @@ const Calculator = () => {
           <Text color="red">Toggle theme</Text>
         </Stack>
 
-        <Display value={399.981} />
+        <Display value={data} />
 
         <Stack
           backgroundColor="gray.800"
@@ -23,31 +26,32 @@ const Calculator = () => {
           padding="1.6rem"
         >
           <Grid gap={3} templateColumns="repeat(4, 1fr)">
-            <ButtonCalc value="7" />
-            <ButtonCalc value="8" />
-            <ButtonCalc value="9" />
+            <ButtonCalc value="7" onClickEvent={handleClick} />
+            <ButtonCalc value="8" onClickEvent={handleClick} />
+            <ButtonCalc value="9" onClickEvent={handleClick} />
             <ButtonCalc
               background="blue.700"
               color="white"
               fontSize="xl"
               shadowColor="#0c4271"
               value="DEL"
+              onClickEvent={handleClick}
             />
 
-            <ButtonCalc value="4" />
-            <ButtonCalc value="5" />
-            <ButtonCalc value="6" />
-            <ButtonCalc value="+" />
+            <ButtonCalc value="5" onClickEvent={handleClick} />
+            <ButtonCalc value="4" onClickEvent={handleClick} />
+            <ButtonCalc value="6" onClickEvent={handleClick} />
+            <ButtonCalc value="+" onClickEvent={handleClick} />
 
-            <ButtonCalc value="1" />
-            <ButtonCalc value="2" />
-            <ButtonCalc value="3" />
-            <ButtonCalc value="-" />
+            <ButtonCalc value="1" onClickEvent={handleClick} />
+            <ButtonCalc value="2" onClickEvent={handleClick} />
+            <ButtonCalc value="3" onClickEvent={handleClick} />
+            <ButtonCalc value="-" onClickEvent={handleClick} />
 
-            <ButtonCalc value="." />
-            <ButtonCalc value="0" />
-            <ButtonCalc value="/" />
-            <ButtonCalc value="x" />
+            <ButtonCalc value="." onClickEvent={handleClick} />
+            <ButtonCalc value="0" onClickEvent={handleClick} />
+            <ButtonCalc value="/" onClickEvent={handleClick} />
+            <ButtonCalc value="x" onClickEvent={handleClick} />
 
             <GridItem colSpan={2}>
               <ButtonCalc
@@ -56,6 +60,7 @@ const Calculator = () => {
                 fontSize="sm"
                 shadowColor="#0c4271"
                 value="RESET"
+                onClickEvent={handleClick}
               />
             </GridItem>
             <GridItem colSpan={2}>
@@ -65,6 +70,7 @@ const Calculator = () => {
                 fontSize="xs"
                 shadowColor="#962d2d"
                 value="="
+                onClickEvent={handleClick}
               />
             </GridItem>
           </Grid>
