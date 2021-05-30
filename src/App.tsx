d1104b1d76a2d92df93f736221187bb75e54c5b9
Julio Romero <julio_ale21@hotmail.com>
2021-05-30 14:18:06 -0300

    chore: fixed height

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
   return (
     <ThemeContext.Provider value={{ themeNumber, setThemeNumber }}>
       <ChakraProvider theme={currentTheme}>
-        <Stack alignItems="center" backgroundColor="bg_main" height="full">
+        <Stack alignItems="center" backgroundColor="bg_main" height="100%">
           <Calculator />
         </Stack>
       </ChakraProvider>
