cfbbc6cd4a6aaa3bd765dca51ff407177a8e886b
Julio Romero <julio_ale21@hotmail.com>
2021-05-30 14:24:39 -0300

    chore: fixed height and min height

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
   return (
     <ThemeContext.Provider value={{ themeNumber, setThemeNumber }}>
       <ChakraProvider theme={currentTheme}>
-        <Stack alignItems="center" backgroundColor="bg_main" height="100vh">
+        <Stack alignItems="center" backgroundColor="bg_main" height="full" minHeight="full">
           <Calculator />
         </Stack>
       </ChakraProvider>
